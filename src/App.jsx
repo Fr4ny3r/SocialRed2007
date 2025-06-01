3a730d69800bd8aadab3e3550a6d8107165a2a2b
Fr4ny3r <guevarafranyer09@gmail.com>
2025-05-31 21:20:58 -0400

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { TargetBoxForm } from './components/targetBoxForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-
+    <TargetBoxForm/>
     </>
   )
 }
--- src/components/targetBoxForm.jsx
+++ src/components/targetBoxForm.jsx
@@ -1,10 +1,12 @@
 
 
 export function TargetBoxForm(){
-
+return(
     <form>
         <input type="email" name="gmail" id="" />
         <input type="password" name="contr" id="" />
         <button type="submit">enviar</button>
     </form>
+)
+
 }
